4890d2e923e77832082f9aa4afc513337670db55
alireza samadi <alireza.samadi.as.as@gmail.com>
2023-02-16 06:28:00 +0330

    restaurant page

--- Js/Enfold-Restaurant.js
+++ Js/Enfold-Restaurant.js
@@ -44,7 +44,7 @@ document.onreadystatechange = function() {
         siteLoaderEl.style.opacity = "0";
         siteLoaderEl.style.visibility = "hidden";
         bodyEl.style.visibility = "visible";
-    };
+    }
 };
 
 window.addEventListener('scroll',()=>{
@@ -55,7 +55,7 @@ window.addEventListener('scroll',()=>{
     } else {
         headerEl.classList.remove('header-light');
         websiteLogoEl.setAttribute('src','Images/logo-restaurant_white.png');
-    };
+    }
 
     // menu item target
     parts.forEach((part)=>{
@@ -65,9 +65,9 @@ window.addEventListener('scroll',()=>{
                     menuItemLink.classList.add('target');
                 } else {
                     menuItemLink.classList.remove('target');
-                };
+                }
             });
-        };
+        }
     });
 });
 
@@ -89,15 +89,15 @@ const headerSlide = ()=>{
                 headerEl.style.top = `-${headerEl.offsetHeight}px`;
             } else {
                 headerEl.style.top = '0';
-            };
+            }
 
             primaryScroll = scrollNow;
-        };
+        }
 
         // debug
         if (menuEl.classList.contains('show-menu')){
             headerEl.style.top = '0';
-        };
+        }
     });
 };
 // by responsive
@@ -105,15 +105,15 @@ if (window.innerWidth > 768){
     headerFix();
 } else if (window.innerWidth <= 768){
     headerSlide();
-};
+}
 // by resize
 window.addEventListener('resize',()=>{
     if (window.innerWidth > 768){
         headerFix();
-    };
+    }
     if (window.innerWidth <= 768){
         headerSlide();
-    };
+    }
 });
 
 // show menu
@@ -131,11 +131,11 @@ showMenu(menuEl);
 const backgrounds = ['burger-1500x1000.jpg','coffee-1500x1000.jpg','fruit-1500x1000.jpg','pasta-1500x1000.jpg'];
 let num = 0;
 homeBg.style.backgroundImage = `url(Images/${backgrounds[num]})`;
-const changBackground = setInterval(()=>{
+setInterval(()=>{
     num++;
     if (num === backgrounds.length){
         num = 0;
-    };
+    }
     homeBg.style.backgroundImage = `url(Images/${backgrounds[num]})`;
 },5000);
 
@@ -146,20 +146,20 @@ bgAttachments.forEach((bgAttachment)=>{
         if (bgAttachment.parentElement.getBoundingClientRect().top <= window.innerHeight && bgAttachment.parentElement.getBoundingClientRect().bottom >= 0 ){
             let numScroll = Math.round(window.scrollY) - bgAttachment.parentElement.offsetTop - bgAttachment.parentElement.getBoundingClientRect().top;
             num = numScroll / 10;
-        };
+        }
 
         bgAttachment.style.transform=`matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, ${num}, 0, 1)`;
     });
 });
 
 // show element scale/position
-function show(element,className) {
+const show = (element,className)=>{
     element.forEach((showElement)=>{
         showElement.classList.add(className);
         window.addEventListener('scroll',()=>{
             if (showElement.parentElement.getBoundingClientRect().top <= window.innerHeight){
                 showElement.style.display = 'block';
-            };
+            }
         });
     });
 };
@@ -173,7 +173,7 @@ for (let num = 0; num < galleryImgs.length;) {
     let galleryImg = document.querySelector(`.parent-img:nth-child(${num}) .gallery-img`);
 
     galleryImg.style.animationDelay = `0.${num}s`;
-};
+}
 
 // modal
 galleryImgs.forEach((galleryImg,index)=>{
@@ -204,7 +204,7 @@ galleryImgs.forEach((galleryImg,index)=>{
             index--;
             if (index < 0){
                 index = galleryImgs.length - 1;
-            };
+            }
 
             showImg();
         });
@@ -213,7 +213,7 @@ galleryImgs.forEach((galleryImg,index)=>{
             index++;
             if (index > galleryImgs.length - 1){
                 index = 0;
-            };
+            }
 
             showImg();
         });
@@ -230,7 +230,7 @@ const hideModal = (target)=>{
             modalBtnClose.classList.remove('active-btn_close');
         },1000);
     });
-}
+};
 hideModal(modalBtnClose);
 hideModal(modalEl);
 // debug
@@ -254,6 +254,6 @@ window.addEventListener('scroll',()=>{
     } else {
         scrollUpEl.style.display = 'block';
         scrollUpEl.classList.remove('scroll-up-hide');
-    };
+    }
 });
 // THE END
